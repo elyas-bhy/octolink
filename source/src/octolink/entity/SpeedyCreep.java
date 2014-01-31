d705508d6179a76aaf58f93d668d0c687cdbf197
Largaroth <rya.herb@gmail.com>
2014-01-31 17:41:56 +0100

    forgot new file

---
+++ source/src/octolink/entity/SpeedyCreep.java
@@ -0,0 +1,23 @@
+package octolink.entity;
+
+import gameframework.base.SpeedVectorDefaultImpl;
+
+import java.awt.Canvas;
+import java.awt.Point;
+
+public class SpeedyCreep extends Creep {
+	
+	protected static int DEFAULT_SPEED = 12;
+	protected static int DEFAULT_HEALTH = 1;
+
+	public SpeedyCreep(Canvas defaultCanvas) {
+		super(defaultCanvas, "images/ghost.gif");
+		setSpeedVector(new SpeedVectorDefaultImpl(new Point(), DEFAULT_SPEED));
+		setHealth(DEFAULT_HEALTH);
+	}
+	
+	public int getDefaultSpeed() {
+		return DEFAULT_SPEED;
+	}
+
+}
